3d9aca369fcc532517c6dc218fefb1a75cb4037b
Antonio Robles <51507400+Antjrobles@users.noreply.github.com>
2025-04-28 19:14:51 +0200

    Create inde.js

---
+++ inde.js
@@ -0,0 +1,68 @@
+function activityHub() {
+    return {
+      activeTab: "julia",
+      juliaActivities: [
+        {
+          title: "Artes plásticas",
+          description: "Explora tu creatividad con dibujos y pinturas",
+          link: "https://www.educacionplastica.net/",
+          icon: "fas fa-paint-brush",
+          visited: false,
+        },
+        {
+          title: "Juegos de lenguaje",
+          description: "Aprende jugando con palabras y letras",
+          link: "https://www.mundoprimaria.com/juegos-educativos/juegos-lenguaje",
+          icon: "fas fa-book",
+          visited: false,
+        },
+        {
+          title: "Matemáticas divertidas",
+          description: "Juegos para aprender matemáticas",
+          link: "https://www.cokitos.com/juegos-de-matematicas-para-ninos/",
+          icon: "fas fa-calculator",
+          visited: false,
+        },
+        {
+          title: "Exploración animal",
+          description: "Descubre el fascinante mundo de los animales",
+          link: "https://www.nationalgeographic.es/animales",
+          icon: "fas fa-paw",
+          visited: false,
+        },
+        {
+          title: "Cuentos interactivos",
+          description: "Historias divertidas para leer y escuchar",
+          link: "https://www.cuentosinfantiles.net/",
+          icon: "fas fa-book-reader",
+          visited: false,
+        },
+      ],
+      marcoActivities: [
+        {
+          title: "Aprender inglés",
+          description: "Juegos para aprender inglés de forma divertida",
+          link: "https://www.duolingo.com/",
+          icon: "fas fa-language",
+          visited: false,
+        },
+        {
+          title: "Ciencia para niños",
+          description: "Experimentos y curiosidades científicas",
+          link: "https://www.sciencekids.co.nz/",
+        },
+        {
+          title: "Ciencia para niños",
+          description: "Experimentos y curiosidades científicas",
+          link: "https://www.sciencekids.co.nz/",
+        },
+        {
+          title: "Ciencia para niños",
+          description: "Experimentos y curiosidades científicas",
+          link: "https://www.sciencekids.co.nz/",
+        },
+      ],
+    };
+  }
+
+
